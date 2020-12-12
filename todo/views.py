203bde072a551050b53c0cdd1b0c1dc4e48c1d2a
Harun <rai.harun64@gmail.com>
2020-12-12 22:13:46 +0545

    Started Todo App

--- todo/views.py
+++ todo/views.py
@@ -4,4 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def Home(request):
-    return HttpResponse("Welcome to Todo Home Page!")
+    # return HttpResponse("Welcome to Todo Home Page!")
+    context = {
+        
+    }
+    return render(request, 'home.html', context)
