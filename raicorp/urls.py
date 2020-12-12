ba296430c784fa5e9d5f2381f163eda0be9db214
Harun <rai.harun64@gmail.com>
2020-12-13 00:08:33 +0545

    added RaiCorp home page

--- raicorp/urls.py
+++ raicorp/urls.py
@@ -22,9 +22,12 @@ from django.conf.urls.static import static
 from django.views.static import serve
 from django.conf.urls import url
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todo/', include('todo.urls')),
+    path('', views.index, name="index"),
 
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
