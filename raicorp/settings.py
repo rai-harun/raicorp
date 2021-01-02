cf8d67b1ef64204833473459237e72a02a5c9c6d
harun rai <harun rai>
2021-01-02 08:20:20 +0545

    fixed static files loading

--- raicorp/settings.py
+++ raicorp/settings.py
@@ -124,8 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'root')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')    
+]
 
 MESSAGE_TAGS={
 messages.ERROR: 'danger'
--- raicorp/urls.py
+++ raicorp/urls.py
@@ -30,6 +30,6 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('', views.index, name="index"),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
